411d041e19a698e9bcb2db859b6e32b789e89eb7
Hugo Gallardo <hgallardo@checkpt.com>
2025-01-28 15:40:55 -0600

    adding logs message writer

--- src/utils/log_message_utils.py
+++ src/utils/log_message_utils.py
@@ -1,15 +1,29 @@
 import logging  
 import config.settings as settings
 from utils.time_utils import DateUtils
+from utils.file_utils import FileUtils
+import csv
 class LogMessagesUtil:
 
     def __init__(self):
         self.logger = logging.getLogger(__name__)
         self.path = settings.LOGS_MESSAGE_PATH
         self.dateUtils =DateUtils()
+        self.fileUtils = FileUtils()
+        self.writes = None
+        self.file = None
 
-    def create():
-        pass
+    def create(self):
+        date=self.dateUtils.getDateISOFormat
+        self.file= f"{date}.csv"
+        if self.fileUtils.folderExist(self.path) is False:
+              self.fileUtils.createFolderFull(self.path)
 
-    def save(message):
-        pass
+        
+        
+
+
+    def save(self, message):
+        with open(f"{self.path}/{self.file}", mode='w') as file:
+                    self.writer = csv.writer(file)
+                    self.writer.writerow(f"{message} \n")
